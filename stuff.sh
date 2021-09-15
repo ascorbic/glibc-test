a555189c18386686aa3fd390a1699b54fbf3eef7
Matt Kane <matt@gatsbyjs.com>
2021-09-15 15:46:13 +0100

    more

--- stuff.sh
+++ stuff.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ldd ./node_modules/lmdb-store/prebuilds/linux-x64/node.abi83.node
+ldd ./node_modules/msgpackr-extract/build/Release/msgpackr-extract.node
 ls -l ./node_modules/msgpackr-extract/build/**/*.node
