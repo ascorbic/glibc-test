5daef374552068ea3008cce11e9c640679b73174
Matt Kane <matt@gatsbyjs.com>
2021-09-15 15:56:56 +0100

    ls

--- stuff.sh
+++ stuff.sh
@@ -3,3 +3,4 @@ ldd ./node_modules/lmdb-store/prebuilds/linux-x64/node.abi83.node
 objdump -T ./node_modules/lmdb-store/prebuilds/linux-x64/node.abi83.node
 ldd ./node_modules/msgpackr-extract/build/Release/msgpackr-extract.node
 objdump -T ./node_modules/msgpackr-extract/build/Release/msgpackr-extract.node
+ls -l ./node_modules/lmdb-store/**/*.node
