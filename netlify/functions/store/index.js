a8ba4da87d947d2c5e5eaeb2ff9931ad058e77b2
Matt Kane <matt@gatsbyjs.com>
2021-09-16 12:49:05 +0100

    function

--- netlify/functions/store/index.js
+++ netlify/functions/store/index.js
@@ -2,11 +2,12 @@ import { open } from "lmdb-store";
 import os from "os";
 import path from "path";
 export function handler() {
-	const myStore = open({
-		path: path.join(os.tmpdir(), "my-db"),
-	});
-	return {
-		statusCode: 200,
-		body: "hello",
-	};
+  const myStore = open({
+    path: path.join(os.tmpdir(), "my-db"),
+  });
+  console.log({ myStore });
+  return {
+    statusCode: 200,
+    body: "hello",
+  };
 }
